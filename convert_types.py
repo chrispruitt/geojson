377571863c920de9e42fddad22d37d00fe1bff04
Chris Pruitt <chris.pruitt15@gmail.com>
2020-04-30 13:52:24 -0500

    add iowa, cleanup

--- convert_types.py
+++ convert_types.py
@@ -1,25 +1,35 @@
+import sys, getopt
 import json
 
-# TopoJson Format
-# with open('initial.json') as json_file:
-#     data = json.load(json_file)
+def convert_fields(output_file):
+    with open('input.json') as json_file:
+        data = json.load(json_file)
 
-#     for i, obj in enumerate(data['objects']['cb_2015_kentucky_county_20m']['geometries'], start=0):
-#         data['objects']['cb_2015_kentucky_county_20m']['geometries'][i]['properties']['COUNTYFP'] = int(obj['properties']['COUNTYFP'])
-#         data['objects']['cb_2015_kentucky_county_20m']['geometries'][i]['properties']['STATEFP'] = int(obj['properties']['STATEFP'])
-#         data['objects']['cb_2015_kentucky_county_20m']['geometries'][i]['properties']['COUNTYNS'] = int(obj['properties']['COUNTYNS'])
+        for i, obj in enumerate(data['features'], start=0):
+            data['features'][i]['properties']['COUNTYFP'] = int(obj['properties']['COUNTYFP'])
 
-#     with open('result.json', 'w') as fp:
-#         json.dump(data, fp, separators=(',', ':'))
+        with open(output_file, 'w') as fp:
+            # output to json file and remove whitespace
+            json.dump(data, fp, separators=(',', ':'))
 
-# GeoJson Format
-with open('geojson/ky-county-fips.json') as json_file:
-    data = json.load(json_file)
 
-    for i, obj in enumerate(data['features'], start=0):
-        data['features'][i]['properties']['COUNTYFP'] = int(obj['properties']['COUNTYFP'])
+def main(argv):
+    output_file = 'output.json'
+    try:
+        opts, _ = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print('test.py -o <output_file>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('convert_types.py -o <output_file>')
+            sys.exit()
+        elif opt in ("-o", "--ofile"):
+            output_file = arg
 
-    with open('result.json', 'w') as fp:
-        # output to json file and remove whitespace
-        json.dump(data, fp, separators=(',', ':'))
 
+    convert_fields(output_file)
+    print('Output file is "', output_file)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
